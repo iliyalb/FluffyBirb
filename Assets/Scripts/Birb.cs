55bf9965840608f14c306941dd75ef9d47abdbdc
ILIYA LESANI <iliyalesani@gmail.com>
2023-01-02 09:44:58 +0330

    Create lose condition

--- Assets/Scripts/Birb.cs
+++ Assets/Scripts/Birb.cs
@@ -4,20 +4,40 @@ using UnityEngine;
 
 public class Birb : MonoBehaviour
 {
-    public Rigidbody2D rigidbodyObject;
+    public bool gameOverState = false;
     public float jumpVelocity;
+    public Rigidbody2D rigidbodyObject;
+    public ScoreManager scoreManager;
     // Start is called before the first frame update
     void Start()
     {
-
+        rigidbodyObject.constraints = RigidbodyConstraints2D.None;
+        scoreManager = GameObject.FindWithTag("GameController").GetComponent<ScoreManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !gameOverState)
         {
             rigidbodyObject.velocity = Vector2.up * jumpVelocity;
         }
+    }
+
+    void EndGame()
+    {
+        scoreManager.GameOver();
+        gameOverState = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        EndGame();
+    }
+
+    private void OnBecameInvisible()
+    {
+        rigidbodyObject.constraints = RigidbodyConstraints2D.FreezeAll;
+        EndGame();
     }
 }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -7,16 +7,23 @@ public class ScoreManager : MonoBehaviour
 {
     public int levelScore;
     public Text textObject;
+    public GameObject gameOverObject;
     // Start is called before the first frame update
     void Start()
     {
         textObject.text = "Score: 0";
+        gameOverObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    public void GameOver()
+    {
+        gameOverObject.SetActive(true);
     }
 
     [ContextMenu("Add Score")]
