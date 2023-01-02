f88c7cd46c1f6a48318c0fc60ff766a287d3e581
ILIYA LESANI <iliyalesani@gmail.com>
2023-01-02 08:03:21 +0330

    Update SpikeSpawn

--- Assets/Scripts/SpikeSpawn.cs
+++ Assets/Scripts/SpikeSpawn.cs
@@ -10,7 +10,7 @@ public class SpikeSpawn : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SpawnPrefab();
+
     }
 
     // Update is called once per frame
