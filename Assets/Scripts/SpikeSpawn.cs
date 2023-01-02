b4637298e8ff67c8ae57c1cdb83dd7a0c7865899
ILIYA LESANI <iliyalesani@gmail.com>
2023-01-02 07:20:24 +0330

    Instantiate spikes

---
+++ Assets/Scripts/SpikeSpawn.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeSpawn : MonoBehaviour
+{
+    public GameObject spawnObject;
+    public float spawnDelay = 3;
+    private float spawnTimer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnPrefab();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnTimer < spawnDelay)
+        {
+            spawnTimer += Time.deltaTime;
+        }
+        else
+        {
+            SpawnPrefab();
+            spawnTimer = 0;
+        }
+    }
+
+    void SpawnPrefab()
+    {
+        Instantiate(spawnObject, new Vector3(transform.position.x, transform.position.y, 0), transform.rotation);
+        
+    }
+}
+
